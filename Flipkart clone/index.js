5de5bee51e270a3fddd9ecfbb33c5f70be1664a7
Parikshit Kumar (─‿‿─) <abha.bharti35@gmail.com>
2023-01-21 23:57:34 +0530

    Add files via upload

--- Flipkart clone/index.js
+++ Flipkart clone/index.js
@@ -7,7 +7,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 4.5,
-    price:"₹1600"
+    price: "1600",
+    id: 1,
+    quantity: 1,
   },
   {
     Name: "Heels",
@@ -15,7 +17,9 @@ ProductData = [
     range: "₹500-1000",
     type: "Accessories",
     rating: 3.2,
-    price:"₹16000"
+    price: "16000",
+    id: 2,
+    quantity: 1,
   },
   {
     Name: "Bycycles",
@@ -23,7 +27,9 @@ ProductData = [
     range: "₹500-700",
     type: "vehicles",
     rating: 4.6,
-    price:"₹17000"
+    price: "17000",
+    id: 3,
+    quantity: 1,
   },
   {
     Name: "Cabinet",
@@ -31,7 +37,9 @@ ProductData = [
     range: "₹500-700",
     type: "furniture",
     rating: 4.4,
-    price:"₹4500"
+    price: "4500",
+    id: 4,
+    quantity: 1,
   },
   {
     Name: "Camera",
@@ -39,7 +47,9 @@ ProductData = [
     range: "₹500-700",
     type: "gadgets",
     rating: 2.5,
-    price:"₹1300"
+    price: "1300",
+    id: 5,
+    quantity: 1,
   },
   {
     Name: "Cargo",
@@ -47,7 +57,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 4.6,
-    price:"₹1500"
+    price: "1500",
+    id: 6,
+    quantity: 1,
   },
   {
     Name: "Tops",
@@ -55,7 +67,9 @@ ProductData = [
     range: "₹500-700",
     type: "Kitchen accessories",
     rating: 2.9,
-    price:"₹16000"
+    price: "16000",
+    id: 7,
+    quantity: 1,
   },
   {
     Name: "Shirt",
@@ -63,7 +77,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 5,
-    price:"₹13000"
+    price: "13000",
+    id: 8,
+    quantity: 1,
   },
   {
     Name: "shirt",
@@ -71,7 +87,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 4.9,
-    price:"₹500"
+    price: "500",
+    id: 9,
+    quantity: 1,
   },
   {
     Name: "Cooling Pad",
@@ -79,7 +97,9 @@ ProductData = [
     range: "₹500-700",
     type: "Gadgets",
     rating: 2.8,
-    price:"₹1200"
+    price: "1200",
+    id: 10,
+    quantity: 1,
   },
   {
     Name: "Data card",
@@ -87,10 +107,9 @@ ProductData = [
     range: "₹100-400",
     type: "gadget",
     rating: 4.6,
-    price:"₹200"
-    
-    
-
+    price: "200",
+    id: 11,
+    quantity: 1,
   },
   {
     Name: "Football",
@@ -98,7 +117,9 @@ ProductData = [
     range: "₹500-700",
     type: "Sports",
     rating: 4.8,
-    price:"₹500"
+    price: "500",
+    id: 12,
+    quantity: 1,
   },
   {
     Name: "Shoes",
@@ -106,7 +127,9 @@ ProductData = [
     range: "₹1500-1700",
     type: "shoes",
     rating: 2.3,
-    price:"₹200"
+    price: "200",
+    id: 13,
+    quantity: 1,
   },
   {
     Name: "Hand Blender",
@@ -114,7 +137,9 @@ ProductData = [
     range: "₹500-700",
     type: "Kitchen accessories",
     rating: 2.9,
-    price:"₹1200"
+    price: "1200",
+    id: 14,
+    quantity: 1,
   },
   {
     Name: "Heels",
@@ -122,7 +147,9 @@ ProductData = [
     range: "₹5000-7000",
     type: "shoes",
     rating: 2.6,
-    price:"₹12000"
+    price: "12000",
+    id: 15,
+    quantity: 1,
   },
   {
     Name: "Jeans",
@@ -130,7 +157,9 @@ ProductData = [
     range: "₹1500-1700",
     type: "cloth",
     rating: 4.5,
-    price:"₹1200"
+    price: "1200",
+    id: 16,
+    quantity: 1,
   },
   {
     Name: "Jeans",
@@ -138,7 +167,9 @@ ProductData = [
     range: "₹400-1700",
     type: "cloth",
     rating: 1.9,
-    price:"₹1200"
+    price: "1200",
+    id: 17,
+    quantity: 1,
   },
   {
     Name: "Kids Clothes",
@@ -146,7 +177,9 @@ ProductData = [
     range: "₹1500-1700",
     type: "cloth",
     rating: 2.9,
-    price:"₹1200"
+    price: "1200",
+    id: 18,
+    quantity: 1,
   },
   {
     Name: "Hand Bangs",
@@ -154,7 +187,9 @@ ProductData = [
     range: "₹500-700",
     type: "Bags",
     rating: 4.5,
-    price:"₹600"
+    price: "600",
+    id: 19,
+    quantity: 1,
   },
   {
     Name: "Led tv",
@@ -162,7 +197,9 @@ ProductData = [
     range: "₹15000-17000",
     type: "Furniture",
     rating: 3.9,
-    price:"₹12000"
+    price: "12000",
+    id: 20,
+    quantity: 1,
   },
   {
     Name: "Mouse",
@@ -170,7 +207,9 @@ ProductData = [
     range: "₹500-700",
     type: "Computer accessories",
     rating: 2.6,
-    price:"₹500"
+    price: "500",
+    id: 21,
+    quantity: 1,
   },
   {
     Name: "Pants",
@@ -178,7 +217,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 4.8,
-    price:"₹600"
+    price: "600",
+    id: 22,
+    quantity: 1,
   },
   {
     Name: "Camera",
@@ -186,7 +227,9 @@ ProductData = [
     range: "₹50000-70000",
     type: "gadgets",
     rating: 4.6,
-    price:"₹60000"
+    price: "60000",
+    id: 23,
+    quantity: 1,
   },
   {
     Name: "Shirt",
@@ -194,7 +237,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 2.6,
-    price:"₹550"
+    price: "550",
+    id: 24,
+    quantity: 1,
   },
   {
     Name: "Speakers",
@@ -202,7 +247,9 @@ ProductData = [
     range: "₹500-700",
     type: "Gadgets",
     rating: 5,
-    price:"₹800"
+    price: "800",
+    id: 25,
+    quantity: 1,
   },
   {
     Name: "Sarees",
@@ -210,7 +257,9 @@ ProductData = [
     range: "₹5000-7000",
     type: "cloth",
     rating: 2.9,
-    price:"₹6000"
+    price: "6000",
+    id: 26,
+    quantity: 1,
   },
   {
     Name: "Salwar",
@@ -218,7 +267,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 1.6,
-    price:"₹800"
+    price: "800",
+    id: 27,
+    quantity: 1,
   },
   {
     Name: "Salwar",
@@ -226,7 +277,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 2.9,
-    price:"₹600"
+    price: "600",
+    id: 28,
+    quantity: 1,
   },
   {
     Name: "Shirt",
@@ -234,7 +287,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 3.1,
-    price:"₹700"
+    price: "700",
+    id: 29,
+    quantity: 1,
   },
   {
     Name: "Shoes",
@@ -242,7 +297,9 @@ ProductData = [
     range: "₹500-700",
     type: "shoes",
     rating: 3.5,
-    price:"₹600"
+    price: "600",
+    id: 30,
+    quantity: 1,
   },
   {
     Name: "Shirts",
@@ -250,7 +307,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 3.9,
-    price:"₹500"
+    price: "500",
+    id: 31,
+    quantity: 1,
   },
   {
     Name: "Suits",
@@ -258,7 +317,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 3.8,
-    price:"₹600"
+    price: "600",
+    id: 32,
+    quantity: 1,
   },
   {
     Name: "Furniture",
@@ -266,7 +327,9 @@ ProductData = [
     range: "₹500-700",
     type: "Furniture",
     rating: 4.5,
-    price:"₹700"
+    price: "700",
+    id: 33,
+    quantity: 1,
   },
   {
     Name: "T-shirt",
@@ -274,7 +337,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 2.6,
-    price:"₹600"
+    price: "600",
+    id: 34,
+    quantity: 1,
   },
   {
     Name: "Tables",
@@ -282,7 +347,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 2.5,
-    price:"₹600"
+    price: "600",
+    id: 35,
+    quantity: 1,
   },
   {
     Name: "Top",
@@ -290,7 +357,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 2.5,
-    price:"₹500"
+    price: "500",
+    id: 36,
+    quantity: 1,
   },
   {
     Name: "Travel bags",
@@ -298,7 +367,9 @@ ProductData = [
     range: "₹500-700",
     type: "bags",
     rating: 2.6,
-    price:"₹600"
+    price: "600",
+    id: 37,
+    quantity: 1,
   },
   {
     Name: "Trousers",
@@ -306,7 +377,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 2.6,
-    price:"₹550"
+    price: "550",
+    id: 38,
+    quantity: 1,
   },
   {
     Name: "T-shirts",
@@ -314,7 +387,9 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 5,
-    price:"₹600"
+    price: "600",
+    id: 39,
+    quantity: 1,
   },
   {
     Name: "Cargo",
@@ -322,14 +397,20 @@ ProductData = [
     range: "₹500-700",
     type: "cloth",
     rating: 2.5,
-    price:"₹650"
+    price: "650",
+    id: 40,
+    quantity: 1,
   },
 ];
 
 let body = document.querySelector(".body");
 let removebutton = document.querySelector(".button-cross");
 let loginpage = document.querySelector(".loginpage");
-
+let search_button = document.querySelector(".btn>button:nth-child(1)");
+let search_input = document.querySelector(".mid>input");
+search_button.addEventListener("click", () => {
+  localStorage.setItem("search_input", JSON.stringify(search_input.value));
+});
 // hide login button
 removebutton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -358,9 +439,6 @@ pageLogin.addEventListener("click", () => {
 
 // sorters and filters
 
-
-
-
 // product display
 let product_banner1 = document.querySelector(".products-slide1");
 let product_banner2 = document.querySelector(".products-slide2");
@@ -370,139 +448,164 @@ let product_banner5 = document.querySelector(".products-slide5");
 let product_banner6 = document.querySelector(".products-slide6");
 display_function = () => {
   ProductData.forEach((element, index) => {
-    
-
-
     if (index < 5) {
-      let anchor=document.createElement("a");
+      let anchor = document.createElement("a");
       let div = document.createElement("div");
-
 
       let img = document.createElement("img");
       let name = document.createElement("h5");
       let price = document.createElement("p");
       let type = document.createElement("p");
-      
-      div.addEventListener("click",()=>{
-      anchor.href="./product.html"
-     anchor.target="new"
-     productLsdata=element
-     localStorage.setItem("product",JSON.stringify(productLsdata))
-     
 
-      })
-      
+      div.addEventListener("click", () => {
+        anchor.href = "./product.html";
+        anchor.target = "new";
+        productLsdata = element;
+        localStorage.setItem("product", JSON.stringify(productLsdata));
+      });
 
       img.src = element.image;
-      name.textContent=element.Name;
-      price.textContent=element.range
-      price.style.color="#388e3c";
-      type.textContent=element.type;
-      btn.innerText="VIEW ALL"
+      name.textContent = element.Name;
+      price.textContent = element.range;
+      price.style.color = "#388e3c";
+      type.textContent = element.type;
+      btn.innerText = "VIEW ALL";
 
-
-    
-      anchor.append(img,name,price,type)
+      anchor.append(img, name, price, type);
       div.append(anchor);
       product_banner1.append(div);
     }
-    if (index >= 5&&index<10) {
+    if (index >= 5 && index < 10) {
+      let anchor = document.createElement("a");
       let div = document.createElement("div");
-      
+
       let img = document.createElement("img");
       let name = document.createElement("h5");
       let price = document.createElement("p");
       let type = document.createElement("p");
-      
-      
-      img.src = element.image;
-      name.textContent=element.Name;
-      price.textContent=element.range
-      price.style.color="#388e3c";
-      type.textContent=element.type;
-      btn.innerText="VIEW ALL"
 
-      div.append(img,name,price,type);
+      div.addEventListener("click", () => {
+        anchor.href = "./product.html";
+        anchor.target = "new";
+        productLsdata = element;
+        localStorage.setItem("product", JSON.stringify(productLsdata));
+      });
+
+      img.src = element.image;
+      name.textContent = element.Name;
+      price.textContent = element.range;
+      price.style.color = "#388e3c";
+      type.textContent = element.type;
+      btn.innerText = "VIEW ALL";
+
+      anchor.append(img, name, price, type);
+      div.append(anchor);
       product_banner2.append(div);
     }
-     if (index >= 10&&index<15) {
+    if (index >= 10 && index < 15) {
+      let anchor = document.createElement("a");
       let div = document.createElement("div");
 
       let img = document.createElement("img");
       let name = document.createElement("h5");
       let price = document.createElement("p");
       let type = document.createElement("p");
-      
+      div.addEventListener("click", () => {
+        anchor.href = "./product.html";
+        anchor.target = "new";
+        productLsdata = element;
+        localStorage.setItem("product", JSON.stringify(productLsdata));
+      });
 
       img.src = element.image;
-      name.textContent=element.Name;
-      price.textContent=element.range
-      price.style.color="#388e3c";
-      type.textContent=element.type;
-      btn.innerText="VIEW ALL"
+      name.textContent = element.Name;
+      price.textContent = element.range;
+      price.style.color = "#388e3c";
+      type.textContent = element.type;
+      btn.innerText = "VIEW ALL";
 
-      div.append(img,name,price,type);
+      anchor.append(img, name, price, type);
+      div.append(anchor);
       product_banner3.append(div);
     }
-     if (index >= 15&&index<19) {
+    if (index >= 15 && index < 19) {
+      let anchor = document.createElement("a");
       let div = document.createElement("div");
 
       let img = document.createElement("img");
       let name = document.createElement("h5");
       let price = document.createElement("p");
       let type = document.createElement("p");
-      
 
+      div.addEventListener("click", () => {
+        anchor.href = "./product.html";
+        anchor.target = "new";
+        productLsdata = element;
+        localStorage.setItem("product", JSON.stringify(productLsdata));
+      });
       img.src = element.image;
-      name.textContent=element.Name;
-      price.textContent=element.range
-      price.style.color="#388e3c";
-      type.textContent=element.type;
-      btn.innerText="VIEW ALL"
+      name.textContent = element.Name;
+      price.textContent = element.range;
+      price.style.color = "#388e3c";
+      type.textContent = element.type;
+      btn.innerText = "VIEW ALL";
 
-      div.append(img,name,price,type);
+      anchor.append(img, name, price, type);
+      div.append(anchor);
       product_banner4.append(div);
     }
-     if (index >= 20&&index<25) {
+    if (index >= 20 && index < 25) {
+      let anchor = document.createElement("a");
       let div = document.createElement("div");
 
       let img = document.createElement("img");
       let name = document.createElement("h5");
       let price = document.createElement("p");
       let type = document.createElement("p");
-      
+      div.addEventListener("click", () => {
+        anchor.href = "./product.html";
+        anchor.target = "new";
+        productLsdata = element;
+        localStorage.setItem("product", JSON.stringify(productLsdata));
+      });
 
       img.src = element.image;
-      name.textContent=element.Name;
-      price.textContent=element.range
-      price.style.color="#388e3c";
-      type.textContent=element.type;
-      btn.innerText="VIEW ALL"
+      name.textContent = element.Name;
+      price.textContent = element.range;
+      price.style.color = "#388e3c";
+      type.textContent = element.type;
+      btn.innerText = "VIEW ALL";
 
-      div.append(img,name,price,type);
+      anchor.append(img, name, price, type);
+      div.append(anchor);
       product_banner5.append(div);
     }
-     if (index >= 25&&index<30) {
+    if (index >= 25 && index < 30) {
+      let anchor = document.createElement("a");
       let div = document.createElement("div");
 
       let img = document.createElement("img");
       let name = document.createElement("h5");
       let price = document.createElement("p");
       let type = document.createElement("p");
-      
+      div.addEventListener("click", () => {
+        anchor.href = "./product.html";
+        anchor.target = "new";
+        productLsdata = element;
+        localStorage.setItem("product", JSON.stringify(productLsdata));
+      });
 
       img.src = element.image;
-      name.textContent=element.Name;
-      price.textContent=element.range
-      price.style.color="#388e3c";
-      type.textContent=element.type;
-      btn.innerText="VIEW ALL"
+      name.textContent = element.Name;
+      price.textContent = element.range;
+      price.style.color = "#388e3c";
+      type.textContent = element.type;
+      btn.innerText = "VIEW ALL";
 
-      div.append(img,name,price,type);
+      anchor.append(img, name, price, type);
+      div.append(anchor);
       product_banner6.append(div);
     }
-
-
   });
 };
 display_function();
